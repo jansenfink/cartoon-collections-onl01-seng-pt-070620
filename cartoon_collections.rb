6b542803fd8b1d9c755468d1dbf54722f4da9177
Jansen Fink <jansenfink@charter.net>
2020-07-18 18:24:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+  
 end
